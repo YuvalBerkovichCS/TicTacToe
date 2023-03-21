a89469a44cc21666e91f538446dceece63b394f6
Yuval <YuvalBerkovichCS@gmail.com>
2023-03-21 15:15:29 +0200

    Rewrote board to use 2 loops
    second task:
    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -30,25 +30,27 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const boardMatrix = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
   return (
-    <>
+    <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
+      {boardMatrix.map((boardArray, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {boardArray.map((squareNum) => (
+            <Square
+              key={squareNum}
+              value={squares[squareNum]}
+              onSquareClick={() => handleClick(squareNum)}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
 
